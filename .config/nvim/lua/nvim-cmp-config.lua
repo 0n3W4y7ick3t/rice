c32bf3eeaeea6a0d2c48068f15cfc16898d34813
leon <leonsatoshi39@gmail.com>
2024-01-15 16:56:01 +0900

    use another rust plugin for nvim

--- .config/nvim/lua/nvim-cmp-config.lua
+++ .config/nvim/lua/nvim-cmp-config.lua
@@ -47,6 +47,28 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
+    ["<Down>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-next)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<Up>"] = cmp.mapping(function()
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+      feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+
   },
 
   sources = cmp.config.sources({

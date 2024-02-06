49c0f917efec772a32c523993a7be603bd34e70b
leon <leonsatoshi39@gmail.com>
2024-02-06 18:02:58 +0900

    more tree-sitter magic, rainbow delimiters

--- .config/nvim/lua/nvim-cmp-config.lua
+++ .config/nvim/lua/nvim-cmp-config.lua
@@ -1,10 +1,10 @@
 local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
 local cmp = require('cmp')
@@ -12,7 +12,7 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     end,
   },
 
@@ -40,10 +40,10 @@ cmp.setup({
     end, { "i", "s" }),
 
     ["<S-Tab>"] = cmp.mapping(function()
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-      feedkey("<Plug>(vsnip-jump-prev)", "")
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
     end, { "i", "s" }),
 
@@ -62,10 +62,10 @@ cmp.setup({
     end, { "i", "s" }),
 
     ["<c-p>"] = cmp.mapping(function()
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-      feedkey("<Plug>(vsnip-jump-prev)", "")
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
     end, { "i", "s" }),
 
@@ -74,9 +74,9 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
-    { name = "nvim_lua", include_deprecated = true },
+    { name = "nvim_lua",               include_deprecated = true },
     { name = 'treesitter' },
-    { name = 'path', option = { trailing_slash = true }},
+    { name = 'path',                   option = { trailing_slash = true } },
     { name = 'vsnip' }, -- For vsnip users.
     { name = 'buffer' },
   }),
@@ -98,4 +98,3 @@ cmp.setup.cmdline({ '/', '?' }, {
     { name = 'buffer' }
   }
 })
-
--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -52,8 +52,8 @@ require 'nvim-treesitter.configs'.setup {
     enable = true,
     keymaps = {
       init_selection = "<CR>", -- set to `false` to disable one of the mappings
-      node_incremental = "gn",
-      node_decremental = "gp",
+      node_incremental = "<CR>",
+      node_decremental = "<BS>",
       scope_incremental = "gs",
     },
   },
@@ -109,6 +109,47 @@ require 'nvim-treesitter.configs'.setup {
       -- and should return true of false
       include_surrounding_whitespace = false,
     },
-  },
 
+    -- set the move motion, most of them are already mapped by vim-unimpaired
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        -- ["]m"] = "@function.outer",
+        -- ["]]"] = { query = "@class.outer", desc = "Next class start" },
+        --
+        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+        -- ["]o"] = "@loop.*",
+        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        ["]]"] = { query = "@function.outer", desc = "Next function start" },
+        ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      },
+      -- goto_next_end = {
+      --   ["]M"] = "@function.outer",
+      --   ["]["] = "@class.outer",
+      -- },
+      goto_previous_start = {
+        --   ["[m"] = "@function.outer",
+        --   ["[["] = "@class.outer",
+        ["[["] = { query = "@function.outer", desc = "Prev function start" },
+        ["[z"] = { query = "@fold", query_group = "folds", desc = "Prev fold" },
+      },
+      -- goto_previous_end = {
+      --   ["[M"] = "@function.outer",
+      --   ["[]"] = "@class.outer",
+      -- },
+      -- -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next = {
+        ["]d"] = { query = "@conditional.outer", desc = "Next conditional" },
+      },
+      goto_previous = {
+        ["[d"] = { query = "@conditional.outer", desc = "Prev conditional" },
+      }
+    },
+  },
 }

a4e0d4c643f6b0fb0050d17f6867ee30e3a64b20
leon <leonsatoshi39@gmail.com>
2024-02-08 12:55:00 +0900

    zig, bash, markdown lsp

--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -15,7 +15,7 @@ require 'treesitter-context'.setup {
 
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "cpp", "python", "go", "bash", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "c", "cpp", "python", "go", "bash", "lua", "vim", "vimdoc", "query", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

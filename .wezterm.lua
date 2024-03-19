418b1b9b148177844d03049aa9401a46ec8a4694
Leon <118800175+0n3W4y7ick3t@users.noreply.github.com>
2024-03-19 18:03:18 +0900

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,8 +11,8 @@ config.font_size = 16.0
 config.window_background_opacity = 0.9
 config.window_decorations = "NONE"
 config.enable_tab_bar = false
-config.font = wezterm.font('MonoLisa', {})
-config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+config.font = wezterm.font('FiraCode Nerd Font', {})
+config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1', 'ss03','ss07', 'ss09', 'cv25', 'cv26', 'cv32' }
 
 -- and finally, return the configuration to wezterm
 return config

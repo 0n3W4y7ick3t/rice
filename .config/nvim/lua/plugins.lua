98fd184065e5e98eb4e3eaa3aed1ba19664e982b
Leon <leonsatoshi39@gmail.com>
2024-10-06 23:19:54 +0000

    update python lsp config, drop pyright

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -387,7 +387,7 @@ require('lazy').setup({
         'golangci_lint_ls',
         'lua_ls',
         'ruff_lsp',
-        'pyright',
+        -- 'basedpyright',
         'marksman',
         'zls',
       }
@@ -413,6 +413,34 @@ require('lazy').setup({
           "--function-arg-placeholders=false", -- disable annoying arguments placing
           "--fallback-style=llvm",
         },
+      }
+
+      lspconfig.pylsp.setup {
+        on_attach = my_attach,
+        settings = {
+          pylsp = {
+            plugins = {
+              -- formatter options
+              black = { enabled = true },
+              autopep8 = { enabled = false },
+              yapf = { enabled = false },
+              -- linter options
+              pylint = { enabled = true, executable = "pylint" },
+              pyflakes = { enabled = false },
+              pycodestyle = { enabled = false },
+              -- type checker
+              pylsp_mypy = { enabled = true },
+              -- auto-completion options
+              jedi_completion = { fuzzy = true },
+              -- import sorting
+              pyls_isort = { enabled = true },
+            }
+          }
+        },
+        flags = {
+          debounce_text_changes = 200,
+        },
+        capabilities = capabilities,
       }
     end
   },
